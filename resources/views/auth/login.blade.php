8b80766d5c0402ac24c13614b729fe2ed8346a5c
Andaru Triadi <54014525+andarutr@users.noreply.github.com>
2022-10-10 11:57:38 +0700

    Authenticate User

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,61 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/login">
+                <img src="/sinpus/assets/images/icons/android-icon-144x144.png">
+            </a>
+            <center>    
+                <h1 style="font-size: 25px; margin-top: 10px;">Login</h1>
+            </center>
+        </x-slot>
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('EMAIL')" />
+
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('PASSWORD')" />
+
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm" href="{{ route('password.request') }}" style="color: blue;">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-primary-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
+        </form>
+        <p>Belum memiliki akun? <a href="/register" style="color: blue;">Register</a></p>
+    </x-auth-card>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
-<?php use Illuminate\Support\Facades\Route; 
-use App\Http\Controllers\AuthController; 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\BookController;
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SinpusController;
 
 /*
@@ -16,14 +15,9 @@ use App\Http\Controllers\SinpusController;
 */
 
 Route::get('/', [SinpusController::class, 'index']);
-Route::get('/kategori', [SinpusController::class, 'category']);
-Route::get('/buku', [SinpusController::class, 'book']);
-Route::get('/buku/atomic-habit', [BookController::class, 'detail']);
-Route::get('/user/profile', [UserController::class, 'profile']);
-Route::get('/user/profile/edit', [UserController::class, 'edit_profile']);
-Route::get('/user/change-password', [UserController::class, 'change_password']);
-Route::get('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'register']);
-Route::get('/forget-password', [AuthController::class, 'forget_password']);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+require __DIR__.'/auth.php';
